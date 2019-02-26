771b1c9965e139972739d9bce2234038d11166c9
panos3oesp <43377896+panos3oesp@users.noreply.github.com>
2019-02-26 18:55:55 +0200

    Update main.py

--- kodikas/main.py
+++ kodikas/main.py
@@ -1,1 +1,8 @@
-import configuration
+from  configuration import *
+from  models.personmodel import *
+
+confManager =  ConfigurationManager("11:00","8:00","res/robot2.sqlite","res/images/faces/")
+print("Hello World")
+personModel=PersonModel(confManager.dbPath)
+persons = personModel.getAll()
+print(persons)
