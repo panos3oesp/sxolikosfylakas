ed572adc814fe1125517882dd4520e8c48167636
panos3oesp <43377896+panos3oesp@users.noreply.github.com>
2019-05-04 17:27:41 +0300

    Update CommunicationsManager.py

--- kodikas/Helpers/CommunicationsManager.py
+++ kodikas/Helpers/CommunicationsManager.py
@@ -1,12 +1,13 @@
+#Κλάση για αποστολή email 
 import smtplib
 class CommunicationsManager:
   def __init__(self,email,adminMail,mailPassword):
-    self.email = email
-    self.adminMail = adminMail,
-    self.mailPassword = mailPassword
+    self.email = email #το εμαιλ αποστολής
+    self.adminMail = adminMail # το email που θα παραλάβει
+    self.mailPassword = mailPassword #το password
   def sendMail(self,message): 
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.login(self.email, self.mailPassword)
-    server.sendmail(self.email, self.adminMail, message)
-    server.quit()
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465) #σύνδεση με τον server
+    server.login(self.email, self.mailPassword)  #κανε login
+    server.sendmail(self.email, self.adminMail, message) #στείλε μήνυμα
+    server.quit() #κλείσε σύνδεση
     
