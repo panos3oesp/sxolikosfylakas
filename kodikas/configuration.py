0885a5f9d7cf18bc85dfb2e359b35896d5887e54
panos3oesp <43377896+panos3oesp@users.noreply.github.com>
2019-05-04 18:18:47 +0300

    Update configuration.py

--- kodikas/configuration.py
+++ kodikas/configuration.py
@@ -1,4 +1,14 @@
-# Κλαση πουτ συγκρατεί τις ρυθμίσεις της εφαρμογής.
+#-------------------------------------------------------------------------------
+# Name:        ConfigurationManager
+# Purpose:     Κλαση πουτ συγκρατεί τις ρυθμίσεις της εφαρμογής. 
+#
+# Author:      μαθητες 3ι εσπερινό ΕΠΑΛ Ν. Φιλαδέφειας
+#
+# Created:     22/02/2019
+# Copyright:   (c) mathitis 2019
+# Licence:     ΜΙΤ
+#-------------------------------------------------------------------------------
+
 
 class ConfigurationManager:
   def __init__(self,startGuardingTime,endGuardingTime,dbPath,faceImagesPath):
