841c9486e37c530ecbe5f7e576753d2f9b84a689
panos3oesp <43377896+panos3oesp@users.noreply.github.com>
2019-05-04 18:21:39 +0300

    Update distance.ino

--- kodikas_arduino/distance.ino
+++ kodikas_arduino/distance.ino
@@ -1,3 +1,16 @@
+/*
+#-------------------------------------------------------------------------------
+# Name:        CommunicationsManager
+# Purpose:     Κλάση για αποστολή email 
+#
+# Author:      μαθητες 3ι εσπερινό ΕΠΑΛ Ν. Φιλαδέφειας
+#
+# Created:     22/02/2019
+# Copyright:   (c) mathitis 2019
+# Licence:     ΜΙΤ
+#-------------------------------------------------------------------------------
+*/
+#Set the pins of the sensors as constants
 #define sensor A0 // Sharp IR GP2Y0A41SK0F (4-30cm, analog)
 #define sensor1 A1 // Sharp IR GP2Y0A41SK0F (4-30cm, analog)
 void setup() {
@@ -13,6 +26,7 @@ void loop() {
   int distance1 = 13 * pow(volts1, -1); // worked out from datasheet graph
 
   delay(200); // slow down serial port
+  //if in small distance
  if (distance <= 20 || distance1 <= 20 ) {
    Serial.print(String(distance)+","+String(distance1));   // print the distance
     
