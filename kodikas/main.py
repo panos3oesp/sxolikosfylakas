178b6cb66f44e91ecbba6a0be5fb1ef11c782409
panos3oesp <43377896+panos3oesp@users.noreply.github.com>
2019-05-04 17:21:40 +0300

    Update main.py

--- kodikas/main.py
+++ kodikas/main.py
@@ -7,64 +7,53 @@ from datetime import datetime
 import random
 
 
-
+# πάρε τις ρυθμίσεις στο σχετικό class
 confManager = ConfigurationManager("11:00","8:00","res/robot2.sqlite","res/images/faces/")
-mediaHelper = MediaHelper()
-faceRecognition = FaceRecognition(confManager,mediaHelper)
+mediaHelper = MediaHelper() #instance του media manager για ήχο και εικόνα
+faceRecognition = FaceRecognition(confManager,mediaHelper) # instance του face recognition controller
 
-#faceRecognition.faceAnalysis()
-
-'''print("Hello World")
-personModel=PersonModel(confManager.dbPath)
-persons = personModel.getAll()
-print(persons)
-mediaHelper.playStringAsSound("Καλημέρα")'''
-
-
-toDoChoices = ['relax', 'playmusic', 'facerecognition','move','giatre']
-toDoChoices = ['relax', 'playmusic', 'facerecognition','giatre']
-#toDoChoices = ['relax', 'playmusic', 'facerecognition','giatre']
-##toDoChoices = ['move']
+#όλες οι πιθανές καταστάσεις του ρομπότ σε λιστα
+toDoChoices = ['relax', 'playmusic', 'facerecognition','move','giatre'] 
 
 katastasi = "αρχική"
 moveController = MoveController(mediaHelper)
-moveController.stop()
-#faceRecognition.faceAnalysis()
-while True:
-    imeraEbdomadas = int(datetime.now().strftime('%w'))
-    ora = int(datetime.now().strftime('%-H'))
-    lepta = int(datetime.now().strftime('%-M'))
-    print("here")
-    if imeraEbdomadas!=0 and imeraEbdomadas!=6 and ora>=10 and ora <= 23 and False:
-        if katastasi!="βοηθός":
+moveController.stop() # σταμάτα τα μοτερ (αν ειχαν μεινει ανοικτά 
+#faceRecognition.faceAnalysis()  #κάνε ανάλυση των προσώπων της βάσης δεδομένων
+while True:  #για πάτα
+    imeraEbdomadas = int(datetime.now().strftime('%w'))  # πάρε την ημέρα της εβδομάδας
+    ora = int(datetime.now().strftime('%-H')) # πάρε την ώρα 
+    lepta = int(datetime.now().strftime('%-M')) # πάρε το σε ποιο λεπτό είμαστε
+    
+    if imeraEbdomadas!=0 and imeraEbdomadas!=6 and ora>=10 and ora <= 23 and False: # αν ανοικτό το σχολείο
+        if katastasi!="βοηθός": #αν άλλαξε η κατάσταση πες το
             katastasi="βοηθός"
             mediaHelper.playStringAsSound("Μπαίνω σε κατάσταση βοηθού")
             print("βοηθός")
-        tiNaKano = random.choice(toDoChoices)
+        tiNaKano = random.choice(toDoChoices) #πάρε τυχαία κατάσταση
         #pesTi = input("Τι να κάνω; ")
         #tiNaKano = toDoChoices[int(pesTi)]
-        if tiNaKano == 'relax':
+        if tiNaKano == 'relax':  #αν relax κοιμήσου για 10 δεύτερα
             print("relax")
             mediaHelper.playStringAsSound("ας κοιμηθώ λίγο")
             time.sleep(10)
-        elif tiNaKano=='playmusic':
+        elif tiNaKano=='playmusic': #αν playmusic πες το και παίξει μουσική
             print("play music")
             mediaHelper.playStringAsSound("ας παίξω μουσική")
             mediaHelper.playRandomMusic()
-        elif tiNaKano=='facerecognition':
+        elif tiNaKano=='facerecognition':  #αν αναγνώριση προσώπου πες το και αναγνώρισε
             print("face rec")
             mediaHelper.playStringAsSound("θα αναγνωρίσω")
             
             faceRecognition.recognise()
-        elif tiNaKano=='giatre':
+        elif tiNaKano=='giatre': #αν giatre πες γεια σου γιατρέ μου :-)
             print("giatre")
-            mediaHelper.playStringAsSound("γεια σου γιατρέ μου")
-        elif tiNaKano=='testDistances':
+            mediaHelper.playStringAsSound("γεια σου γιατρέ μου")  
+        elif tiNaKano=='testDistances': #αυτό ειναι για έλεγχο μόνο αποστάσεων δεν κανει κάτι κανονικά
             mediaHelper.playStringAsSound("ας ελέγξω αποστάσεις")
             logFile = open("distanceLog.txt","w")
             moveController.testDistances(logFile)
             logFile.close()
-        else:
+        else: # αλλιως πες οτι θα ξεκινήσεις και βάλε τον movecontroller να δώσει ρεύμα στα μοτερ
             mediaHelper.playStringAsSound("ας κάνω μια βολτίτσα")
             print("move")
             logFile = open("distanceLog.txt","w")
@@ -77,52 +66,16 @@ while True:
                 
             
             
-    else:
+    else:  #αν κλειστό σχολείο
         if katastasi!="φύλακας":
            katastasi="φύλακας"
            print("φύλακας")
-           mediaHelper.playStringAsSound("Μπαίνω σε κατάσταση φύλακα")
-           faceRecognition.recognise(True)
+           mediaHelper.playStringAsSound("Μπαίνω σε κατάσταση φύλακα") # πες την κατάσταση
+           faceRecognition.recognise(True) # κάνε αναγνώριση αλλά αν ξένος σήμανε συναγερμό..
 
      
 
 
 
 
-'''
-moveController = MoveController(mediaHelper)
-print("stopping controller")
-moveController.stop()
 
-for i in range(30000):
-    moveController.move()
-    
-    
-moveController.stop()
-'''
-
-
-
-
-
-'''mediaHelper.playStringAsSound("Παω μπροστά")
-print("forward controller")
-for i in range(300000):
-        
-        moveController.forward()
-mediaHelper.playStringAsSound("Παω πίσω")
-print("backward controller")
-for i in range(300000):
-        
-        moveController.reverse()
-mediaHelper.playStringAsSound("Παω αριστερά")
-for i in range(300000):
-        
-        moveController.left()
-mediaHelper.playStringAsSound("Παω δεξιά")
-for i in range(300000):
-        
-        moveController.right()
-
-moveController.stop()
-'''
