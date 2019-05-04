97071980f880f6798e7fd95c51be14979bcb85b8
panos3oesp <43377896+panos3oesp@users.noreply.github.com>
2019-05-04 18:16:36 +0300

    Update mainController.py

--- kodikas/controllers/mainController.py
+++ kodikas/controllers/mainController.py
@@ -1,4 +1,14 @@
-#παλιος controller, θα σβηστεί
+#-------------------------------------------------------------------------------
+# Name:        MainController
+# Purpose:     παλιος controller, θα σβηστεί
+#
+# Author:      μαθητες 3ι εσπερινό ΕΠΑΛ Ν. Φιλαδέφειας
+#
+# Created:     22/02/2019
+# Copyright:   (c) mathitis 2019
+# Licence:     ΜΙΤ
+#-------------------------------------------------------------------------------
+
 import random
 class MainController:
     toDoChoices = ['relax', 'playmusic', 'facerecognition']
