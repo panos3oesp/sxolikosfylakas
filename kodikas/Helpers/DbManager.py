c9a052eb9e8ba2ff2bf4db34b26998f61af18c01
panos3oesp <43377896+panos3oesp@users.noreply.github.com>
2019-05-04 17:29:44 +0300

    Update DbManager.py

--- kodikas/Helpers/DbManager.py
+++ kodikas/Helpers/DbManager.py
@@ -1,11 +1,13 @@
 #-------------------------------------------------------------------------------
-# Name:        module1
+# Name:        DbManager
 # Purpose:
 #
 # Author:      3o Εσπερινο ΕΠΑΛ
 #
 # Created:     22/02/2019
 #-------------------------------------------------------------------------------
+
+#συνδέεται με sqlite3 και τρέχει wql
 import sqlite3
 print (sqlite3.sqlite_version)
 from sqlite3 import Error
@@ -13,28 +15,25 @@ from sqlite3 import Error
 
 class DbManager:
     def __init__(self,filePath):
-        self.filePath=filePath
-        self.lastQuery=''
+        self.filePath=filePath #το μονοπάτι της βάσης
+        self.lastQuery='' #το τελευταίο query
 
         try:
-            self.conn = sqlite3.connect(filePath)
+            self.conn = sqlite3.connect(filePath) #δοκίμασε να συνδεθείς
             #self.conn.execute('''CREATE TABLE Person
             # ([id] INTEGER PRIMARY KEY,[name] text,[surname] text,[imagepath] text, [isauthorised] integer, [lastseen] date)''')
 
 # Create table - COUNTRY
-        except Error as e:
+        except Error as e: #αν λάθος τύπωσε
             print(e)
             print ("Cannot connect to database")
 
-    def runSelect(self,query):
+    def runSelect(self,query): # Εκτέλεση query
         self.lastQuery=query
         cur = self.conn.cursor()
         cur.execute(query)
-        rows = cur.fetchall()
-        return rows
+        rows = cur.fetchall()  #πάρε τις γραμμές
+        return rows #επέστρεψε τις
 
 
 
-#a=DbManager("robot2.sqlite")
-#persons=a.runSelect("SELECT * FROM Person")
-#print (persons)
