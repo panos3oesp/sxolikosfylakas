d20e0f0fe85d743bc9ec9297072071b38f22b857
panos3oesp <43377896+panos3oesp@users.noreply.github.com>
2019-05-04 17:26:03 +0300

    Update onlyStop.py

--- kodikas/onlyStop.py
+++ kodikas/onlyStop.py
@@ -1,5 +1,7 @@
+#Δοκιμαστικός κώδικας για να σταματάει τα μοτερ αν παραμείνουν κλειστά
+
 from controllers.moveController import *
 from Helpers.MediaHelper import *
 mediaHelper = MediaHelper()
 moveController = MoveController(mediaHelper)
-moveController.stop()
+moveController.stop() #κλεισε τα μοτερ
