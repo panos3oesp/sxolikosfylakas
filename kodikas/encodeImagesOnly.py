1d3590637fb0867ca6adfd21467c936578871374
panos3oesp <43377896+panos3oesp@users.noreply.github.com>
2019-05-04 17:12:37 +0300

    Update encodeImagesOnly.py

--- kodikas/encodeImagesOnly.py
+++ kodikas/encodeImagesOnly.py
@@ -1,3 +1,5 @@
+# Κώδικας που τρέχει αυτόνομα και δημιουργεί το αρχείο με τις αναλύσεις των προσώπων που περιέχονται στις φωτογραφίες.
+
 from configuration import *
 from models.personmodel import *
 from Helpers.MediaHelper import *
@@ -6,9 +8,9 @@ from controllers.FaceRecognition import *
 from datetime import datetime
 import random
 
-
+# θέσε τις ρυθμίσεις
 confManager = ConfigurationManager("11:00","8:00","res/robot2.sqlite","res/images/faces/")
-mediaHelper = MediaHelper()
-faceRecognition = FaceRecognition(confManager)
+mediaHelper = MediaHelper() #Κατασκεύασε instance το media manager για ηχο και media γενικότερα
+faceRecognition = FaceRecognition(confManager) #Κατασκεύασε instance το face recognition controller για ανάλυση εικόνων
 print("Start")
-faceRecognition.justEncode()
+faceRecognition.justEncode() #κάνε την ανάλυση των εικόνων
