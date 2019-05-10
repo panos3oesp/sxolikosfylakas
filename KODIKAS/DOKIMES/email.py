ac7ff60e6f2f0e9b6f3d5875610b986fb46bef6b
panos3oesp <43377896+panos3oesp@users.noreply.github.com>
2019-05-10 20:08:41 +0300

    Delete email.py

--- KODIKAS/DOKIMES/email.py
+++
@@ -1,6 +0,0 @@
-import smtplib
-server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-email = "robo3epal@gmail.com"
-server.login(email, "robotaki")
-server.sendmail(email, email, "KALIMERA TI KANETE")
-server.quit()
